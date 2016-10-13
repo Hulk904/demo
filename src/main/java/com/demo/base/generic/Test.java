5724188c1cc797ac258864a212cf1f47f348689d
yangyuan40 <yangyuan3@jd.com>
2016-10-13 17:58:57 +0800

    update

--- src/main/java/com/demo/base/generic/Test.java
+++ src/main/java/com/demo/base/generic/Test.java
@@ -2,18 +2,29 @@ package com.demo.base.generic;
 
 /**
  * Created by yangyuan on 16-9-30.
+ *
  */
 public class Test {
     public static void main(String[] args) {
         try {
-            say(Integer.class);
+            say(String.class);
         }catch (Exception exc){
-            System.out.println(exc);
+            System.out.println("=="+exc);
         }
 
     }
-    public static <T> T say(Class param) throws  Exception{
+
+    /**
+     * T 可用于定义变量
+     * param 是类对象 ,可以实例化生成对象.
+     * @param param
+     * @param <T>
+     * @return
+     * @throws Exception
+     */
+    public static <T> T say(Class<T> param) throws  Exception{
         System.out.println(param);
-        return  (T)param.newInstance();
+        T t = param.newInstance();
+        return  t;
     }
 }
