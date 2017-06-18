88521aae9f83fbab5806dfeef4cbfbdafd9c16a0
yangyuan <yangyuan3@jd.com>
2017-06-18 15:23:18 +0800

    classload test

--- src/main/java/com/demo/base/classloader/ClassLoaderInfo.java
+++ src/main/java/com/demo/base/classloader/ClassLoaderInfo.java
@@ -1,11 +1,19 @@
 package com.demo.base.classloader;
 
+import com.demo.base.serializable.Person;
+
 /**
  * Created by yangyuan on 16-8-23.
  */
 public class ClassLoaderInfo {
-    public static void main(String[] args) {
-        System.out.println(ClassLoaderInfo.class.getClassLoader().toString());
-        ClassLoader.getSystemClassLoader();
+    public static void main(String[] args) throws Exception{
+//        System.out.println(ClassLoaderInfo.class.getClassLoader().toString());
+//        ClassLoader.getSystemClassLoader();
+//        Class clazz = Person.class;//也会加载 Person但是不会执行static快
+//        clazz.getClassLoader();
+//        Class clazz1 = ClassLoaderInfo.class.getClassLoader().loadClass(clazz.getName());//这里并不会执行static快
+//        Class.forName(clazz.getName(),true,ClassLoaderInfo.class.getClassLoader());//true 则会执行static块
+//        System.out.println(clazz == clazz1);//true
+          Person person  ;//只是声明不会加载类
     }
 }
--- src/main/java/com/demo/base/serializable/Person.java
+++ src/main/java/com/demo/base/serializable/Person.java
@@ -6,6 +6,9 @@ import java.io.Serializable;
  * Created by yangyuan on 17-4-18.
  */
 public class Person implements Serializable {
+    static {
+        System.out.println("i'm static!");
+    }
     private String name;
     private int age;
 }
